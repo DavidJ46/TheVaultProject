1da58c7708ab2908205efa0900ca9836e59813fc
RyGrimes <145487186+rygrimes1@users.noreply.github.com>
2026-02-03 16:40:13 -0500

    Update ListingScreen.py

--- ListingScreen.py
+++ ListingScreen.py
@@ -2,4 +2,4 @@
 
 #Import dependencies
   #When an item is clicked, a window for that item to be viewed should be opened
-    #Window should contain item attributes (Item, Author, Date Posted, Description) and a button to purchase it
+    #Window should contain item attributes (Item, Author, Date Posted, Size, Description) and a button to purchase it
