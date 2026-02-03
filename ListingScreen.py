0d773837ad4aae1ba42039a5620d75744d0c608a
RyGrimes <145487186+rygrimes1@users.noreply.github.com>
2026-02-03 16:39:18 -0500

    Create ListingScreen.py

---
+++ ListingScreen.py
@@ -0,0 +1,5 @@
+#Should display items
+
+#Import dependencies
+  #When an item is clicked, a window for that item to be viewed should be opened
+    #Window should contain item attributes (Item, Author, Date Posted, Description) and a button to purchase it
