82aa12a733f1add560b8b3c697aeef9a5306cec7
daytonae <161365857+daytonae@users.noreply.github.com>
2026-02-26 21:43:59 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from controllers.listing_controller import listing_bp
+from controllers.storefront_controller import storefront_bp
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(listing_bp)
+    app.register_blueprint(storefront_bp)
 
     @app.get("/health")
     def health():
